463af25dc331b23af647ef2bac603a13350d90ad
Frank Schenkel <a01701088@itesm.mx>
2021-10-19 15:52:00 -0500

    Added filtering for category admin view

--- Educap/LearningCatalog/admin.py
+++ Educap/LearningCatalog/admin.py
@@ -1,6 +1,23 @@
 from django.contrib import admin
 from .models import *
+import string
 # Register your models here.
 
+class AlphabetFilter(admin.SimpleListFilter):
+    title = 'alfabeto'
+    parameter_name = 'alfabeto'
 
-admin.site.register(Categoria)
+    def lookups(self, request, model_admin):
+        abc = list(string.ascii_lowercase)
+        return ((c.upper(), c.upper()) for c in abc)
+
+    def queryset(self, request, queryset):
+        if self.value():
+            return queryset.filter(Nombre__startswith=self.value())
+
+class CategoriaAdmin(admin.ModelAdmin):
+    list_display = ('Nombre','Descripción','FechaCreada')
+    list_filter = (AlphabetFilter, 'FechaCreada')
+    ordering =('Nombre',)
+
+admin.site.register(Categoria, CategoriaAdmin)
--- Educap/LearningCatalog/models.py
+++ Educap/LearningCatalog/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    Name = models.CharField(max_length=30)
-    Description = models.CharField(max_length=100)
-    DateCreated = models.DateTimeField(auto_now_add=True)
+    Nombre = models.CharField(max_length=30)
+    Descripción = models.CharField(max_length=100)
+    FechaCreada = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.Name}"
+        return f"{self.Nombre}"
