3760cf9795807b198aafebaf9719dcbd65cadd8d
Frank Schenkel <a01701088@itesm.mx>
2021-10-21 17:32:16 -0500

    Catalog Page now properly renders all available catalogs

--- Educap/LearningCatalog/views.py
+++ Educap/LearningCatalog/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render
 from . import urls
 
+from LearningCatalog.models import Categoria
+
 # Create your views here.
 
 def LearningCatalog(request):
+    
     return render(request, "LearningCatalog/primaryCatalog.html",{
-
+        "catalogs": Categoria.objects.all(),
+        
     })
