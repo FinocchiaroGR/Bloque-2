b54c66f07d2ec823ad961bc23579edea75b9975f
Frank Schenkel <a01701088@itesm.mx>
2021-10-21 14:04:08 -0500

    Primary Category key edited

--- Educap/LearningCatalog/models.py
+++ Educap/LearningCatalog/models.py
@@ -4,9 +4,9 @@ from django.db import models
 
 
 class Categoria(models.Model):
-    Nombre = models.CharField(max_length=30)
-    Descripción = models.CharField(max_length=100)
-    FechaCreada = models.DateTimeField(auto_now_add=True)
+    Name = models.CharField(max_length=30)
+    Description = models.CharField(max_length=100)
+    DateCreated = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.Nombre}"
+        return f"{self.pk} {self.Name}"
