a847c06b04845b650657f6d33f72761d93a7b07e
Juan Chalita <chalajr2@gmail.com>
2021-10-31 23:16:04 -0600

    Develop lessons and categories, lessons view and category search bar pending

--- Educap/LearningCatalog/admin.py
+++ Educap/LearningCatalog/admin.py
@@ -3,6 +3,7 @@ from .models import *
 import string
 # Register your models here.
 
+
 class AlphabetFilter(admin.SimpleListFilter):
     title = 'alfabeto'
     parameter_name = 'alfabeto'
@@ -13,11 +14,26 @@ class AlphabetFilter(admin.SimpleListFilter):
 
     def queryset(self, request, queryset):
         if self.value():
-            return queryset.filter(Nombre__startswith=self.value())
+            return queryset.filter(nombre__startswith=self.value())
+
 
 class CategoriaAdmin(admin.ModelAdmin):
-    list_display = ('pk','Nombre','Descripción','FechaCreada')
-    list_filter = (AlphabetFilter, 'FechaCreada')
-    ordering =('Nombre',)
+    list_display = ('pk', 'nombre', 'descripción', 'fechaCreada')
+    list_filter = (AlphabetFilter, 'fechaCreada')
+    ordering = ('nombre',)
+
+
+class ArchivoAdminInline(admin.TabularInline):
+    model = Archivo
+
+
+class VideoAdminInline(admin.TabularInline):
+    model = (Video)
+
+
+class LeccionAdmin(admin.ModelAdmin):
+    inlines = (ArchivoAdminInline, VideoAdminInline)
+
 
 admin.site.register(Categoria, CategoriaAdmin)
+admin.site.register(Leccion, LeccionAdmin)
--- Educap/LearningCatalog/models.py
+++ Educap/LearningCatalog/models.py
@@ -1,13 +1,45 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
 
 # Create your models here.
 
+FILETYPES = [
+    ('PDF', 'Archivo PDF'),
+    ('IMG', 'Imagen'),
+]
+
 
 class Categoria(models.Model):
-    Nombre = models.CharField(max_length=30)
-    Descripción = models.CharField(max_length=100)
-    FechaCreada = models.DateTimeField(auto_now_add=True)
-    SubCategoria = models.ForeignKey('self', blank=True,null=True,on_delete=models.CASCADE)
+    nombre = models.CharField(max_length=30)
+    descripción = models.CharField(max_length=100)
+    fechaCreada = models.DateTimeField(auto_now_add=True)
+    subCategoria = models.ForeignKey(
+        'self', blank=True, null=True, on_delete=models.CASCADE)
 
     def __str__(self):
-        return f"{self.pk} {self.Nombre}"
+        return f"{self.pk} {self.nombre}"
+
+
+class Leccion(models.Model):
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField("Descripción", max_length=500)
+    imagen = models.ImageField(upload_to='uploads/lessons/img')
+    fecha = models.DateField(
+        "Fecha de creación", auto_now=False, auto_now_add=True)
+    aprobacion = models.BooleanField("Aprobación")
+    category = models.ForeignKey(Categoria, on_delete=CASCADE)
+
+
+class Archivo(models.Model):
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField("Descripción", max_length=500)
+    path = models.FileField("Archivo", upload_to='uploads/files')
+    lipo = models.CharField("Tipo de archivo", choices=FILETYPES, max_length=3)
+    leccion = models.ForeignKey(Leccion, on_delete=CASCADE)
+
+
+class Video(models.Model):
+    titulo = models.CharField(max_length=150)
+    descripcion = models.TextField("Descripción", max_length=500)
+    link = models.CharField(max_length=500)
+    leccion = models.ForeignKey(Leccion, on_delete=CASCADE)
