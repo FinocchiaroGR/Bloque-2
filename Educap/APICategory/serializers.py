a745ae1b935ef79481988f7003ca62b281c5ad49
Frank Schenkel <a01701088@itesm.mx>
2021-11-17 16:38:23 -0600

    Develop Get All Categoy API

--- Educap/APICategory/serializers.py
+++ Educap/APICategory/serializers.py
@@ -1,13 +1,11 @@
 from django.contrib.auth.models import User
 from django.db import models
-from Educap.LearningCatalog.models import Categoria
+from LearningCatalog.models import Categoria
 from rest_framework import fields, serializers
 from LearningCatalog.models import UserModel, Estudiante
 
-class GetCategorySerializer(serializers.ModelSerializer):
+class getCategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Categoria
         fields = ['nombre', 'descripcion', 'imagen','categoriaPadre']
         
-    def get():
-        return
