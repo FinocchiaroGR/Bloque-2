26079744fa2fac260d01b91b579fa7e126682ddb
Juan Chalita <chalajr2@gmail.com>
2021-11-17 14:42:53 -0600

    New serializers

--- Educap/APIAccounts/views.py
+++ Educap/APIAccounts/views.py
@@ -2,6 +2,7 @@ from django.conf.urls import handler500
 from django.shortcuts import render
 from rest_framework.views import APIView
 from rest_framework.response import Response
+from rest_framework.permissions import IsAuthenticated
 from .serializers import *
 from rest_framework import status
 import logging
@@ -26,3 +27,20 @@ class RegisterStudentView(APIView):
             return Response(serializer.data, status=status.HTTP_201_CREATED)
         logging.error(serializer.errors)
         return Response(serializer.errors, status=handler500)
+
+
+class GetStudentView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        student = Estudiante.objects.get(user=request.user)
+        serializer = getStudentSerializer(student)
+        return Response(serializer.data)
+
+
+class GetuserView(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def get(self, request):
+        serializer = getUserSerializer(request.user)
+        return Response(serializer.data)
